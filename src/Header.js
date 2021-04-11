dcf40bf31fae38bed75d26fd7beb28361a98f240
adam040992 <adam040992@gmail.com>
2021-04-11 14:21:11 +0200

    Styling Header component

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
     return (
@@ -12,6 +14,31 @@ function Header() {
                 <OptionLineOne>Hello,</OptionLineOne>
                 <OptionLineTwo>Select your address</OptionLineTwo>
             </HeaderOptionAdress>
+
+            <HeaderSearch>
+                <HeaderSearchInput type="text" />
+                <HederSearchIconContainer>
+                    <SearchIcon />
+                </HederSearchIconContainer>
+            </HeaderSearch>
+
+            <HeaderNavItems>
+                <HeaderOption>
+                    <OptionLineOne>Hello, Nazariy</OptionLineOne>
+                    <OptionLineTwo>Account & Lists</OptionLineTwo>
+                </HeaderOption>
+
+                <HeaderOption>
+                    <OptionLineOne>Returns</OptionLineOne>
+                    <OptionLineTwo>& Orders</OptionLineTwo>
+                </HeaderOption>
+
+                <HeaderOptionCart>
+                    <ShoppingBasketIcon />
+                    <CartCount>6</CartCount>
+                </HeaderOptionCart>
+            </HeaderNavItems>
+
         </Container>
     )
 }
@@ -23,20 +50,55 @@ const Container = styled.div`
     background-color: #0F1111;
     display: flex;
     align-items: center;
+    justify-content: space-between;
+    color: white;
 `;
 
 const HeaderLogo = styled.div`
     img {
         width: 100px;
         object-fit: content;
+        margin-left: 11px;
     }
 `;
 
-const HeaderOptionAdress = styled.div`
-
-`;
+const HeaderOptionAdress = styled.div``;
 
 const OptionLineOne = styled.div``;
 
-const OptionLineTwo = styled.div``;
+const OptionLineTwo = styled.div`
+    font-weight: 700;
+`;
+
+const HeaderSearch = styled.div`
+    display: flex;
+    flex-grow: 1;
+    height: 40px;
+    border-radius: 4px;
+`;
+
+const HeaderSearchInput = styled.input`
+    flex-grow: 1;
+`;
+
+const HederSearchIconContainer = styled.div`
+    background-color: #febd69;
+    width: 45px;
+    color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const HeaderNavItems = styled.div`
+    display: flex;
+`;
+
+const HeaderOption = styled.div`
+    padding: 10px 9px 10px 9px;
+`;
+
+const HeaderOptionCart = styled.div``;
+
+const CartCount = styled.div``;
 
