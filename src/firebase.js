32059291d8edab274f766b4b8658776fdaa79e72
adam040992 <adam040992@gmail.com>
2021-04-14 11:30:11 +0200

    Configure firebase db

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = firebase.initializeApp ({
+    apiKey: "AIzaSyAlCleiROVP8oXZTwW_yea7K1rLWX8FkzM",
+    authDomain: "clone-61896.firebaseapp.com",
+    projectId: "clone-61896",
+    storageBucket: "clone-61896.appspot.com",
+    messagingSenderId: "822766775539",
+    appId: "1:822766775539:web:d7b432d58e2e36a8c41b86"
+  });
+
+  const db = firebase.firestore();
+
+  export { db };
